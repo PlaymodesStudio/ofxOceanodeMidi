5f115455ac0fe964c9d91bd4b3f5e75c1cda5cef
Santi_Vilanova <santi@playmodes.com>
2023-05-30 12:12:15 +0200

    Note burst fixed (using change); Added midi cc

--- src/midiOut.h
+++ src/midiOut.h
@@ -1,4 +1,3 @@
-//
 //  midiOut.h
 //  example
 //
@@ -19,29 +18,41 @@ public:
         addParameterDropdown(midiPort, "Port", 0, midiOut.getOutPortList());
         addParameter(midiChannel.set("Ch", 1, 1, 16));
         addParameter(notes.set("Notes", {0}, {0}, {1}));
+        addParameter(cc.set("CC", {0}, {0}, {1}));
         
         listeners.push(midiPort.newListener([this](int &device){
             midiOut.openPort(device);
         }));
         
         listeners.push(notes.newListener([this](vector<float> &vf){
-            if(vf.size() == outputStore.size()){
-                if(vf != outputStore){
-                    for(int i = 0; i < vf.size(); i++){
-                        if(vf[i] != outputStore[i]){
-                            if(vf[i] != 0){
-                                if(outputStore[i] != 0){
-                                    midiOut.sendNoteOff(midiChannel, i);
-                                }
-                                midiOut.sendNoteOn(midiChannel, i, vf[i]*127);
-                            }else{
-                                midiOut.sendNoteOff(midiChannel, i);
-                            }
+            if(vf.size() != notesStore.size()){
+                notesStore.resize(vf.size(), 0.0);
+            }
+            for(int i = 0; i < vf.size(); i++){
+                if(vf[i] != notesStore[i]){
+                    if(vf[i] != 0){
+                        if(notesStore[i] != 0){
+                            midiOut.sendNoteOff(midiChannel, i);
                         }
+                        midiOut.sendNoteOn(midiChannel, i, vf[i]*127);
+                    }else{
+                        midiOut.sendNoteOff(midiChannel, i);
                     }
                 }
             }
-            outputStore = vf;
+            notesStore = vf;
+        }));
+
+        listeners.push(cc.newListener([this](vector<float> &vf){
+            if(vf.size() != ccStore.size()){
+                ccStore.resize(vf.size(), 0.0);
+            }
+            for(int i = 0; i < vf.size(); i++){
+                if(vf[i] != ccStore[i]){
+                    midiOut.sendControlChange(midiChannel, i, vf[i]*127);
+                }
+            }
+            ccStore = vf;
         }));
     }
     
@@ -50,11 +61,13 @@ private:
     ofParameter<int> midiPort;
     ofParameter<int> midiChannel;
     ofParameter<vector<float>> notes;
+    ofParameter<vector<float>> cc;
     
     ofEventListeners listeners;
     
     ofxMidiOut midiOut;
-    vector<float> outputStore;
+    vector<float> notesStore;
+    vector<float> ccStore;
 };
 
 #endif /* midiOut_h */
